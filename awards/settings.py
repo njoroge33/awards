4421334e98eb83dc41cc1fd49156fc26c14ae5d4
njoroge33 <johngichuhi769@gmail.com>
2020-01-12 00:42:20 +0300

    setup database

--- awards/settings.py
+++ awards/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'awards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'awards',
+        'USER': 'moringa',
+        'PASSWORD': 'limuru33',
+        'PORT': '',
     }
 }
 
@@ -107,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
