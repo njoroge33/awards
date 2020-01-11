cf6075bbfacc7fc2174a8e1a80f63e388c350540
njoroge33 <johngichuhi769@gmail.com>
2020-01-12 02:25:31 +0300

    Implement django logout view

--- awardapp/urls.py
+++ awardapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, re_path, include
 from . import views
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', views.signup, name='signup'),
     path('home/', views.index, name='home'),
-    re_path(r'^login/', LoginView.as_view(), name='login')
+    re_path(r'^login/', LoginView.as_view(), name='login'),
+    re_path(r'^logout/', LogoutView.as_view(next_page='login'), name='logout')
 ]
--- awardapp/views.py
+++ awardapp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm
 
 # Create your views here.
-
+@login_required(login_url='/login')
 def index(request):
     return render(request, 'index.html')
 
