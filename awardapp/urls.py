614a925e36b3bb4532b3f85e1c0682e748f37a2b
njoroge33 <johngichuhi769@gmail.com>
2020-01-12 02:15:05 +0300

    add a login page

--- awardapp/urls.py
+++ awardapp/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path, include
+from django.urls import path, re_path, include
 from . import views
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', views.signup, name='signup'),
-    path('home', views.index, name='home'),
+    path('home/', views.index, name='home'),
+    re_path(r'^login/', LoginView.as_view(), name='login')
 ]
