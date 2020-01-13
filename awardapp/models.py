c41ec570536b34f36ac15b6966ab55347035686b
njoroge33 <johngichuhi769@gmail.com>
2020-01-13 13:13:48 +0300

    create a project model

--- awardapp/models.py
+++ awardapp/models.py
@@ -1,5 +1,21 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Projects(models.Model):
-    pass
+class Project(models.Model):
+    project_image = models.ImageField(upload_to = 'projects/')
+    caption = models.CharField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
+    @classmethod
+    def get_projects(cls):
+        projects = cls.objects.all()
+        return projects
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
